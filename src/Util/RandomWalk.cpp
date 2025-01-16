75ab0d1c88b75861173788ae233187432d05df5e
BPelokTUe <b.pelok@student.tue.nl>
2025-01-16 19:26:52 +0100

    Random walk generation

---
+++ src/Util/RandomWalk.cpp
@@ -0,0 +1,44 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <random>
+
+#include "Util/RandomWalk.hpp"
+
+int create_random_walks(std::string dataset_path, float rw_noise, bool zero_start, unsigned num_series,
+                        unsigned series_len, unsigned num_channels) {
+    namespace fs = std::filesystem;
+
+    if (fs::exists(dataset_path)) {
+        std::cerr << "Error: Dataset " << dataset_path << " already exists." << std::endl;
+        return 1;
+    }
+
+    // Create binary file containing the time series
+    std::ofstream outfile(dataset_path, std::ios::binary);
+    if (!outfile) {
+        std::cerr << "Error: Could not create dataset " << dataset_path << '\n';
+        std::cerr << "Reason: " << std::strerror(errno) << std::endl;
+        return 2;
+    }
+
+    std::default_random_engine generator;
+    std::normal_distribution<float> distribution(0.0, rw_noise);
+
+    for (unsigned i = 0; i < num_series; ++i) {
+        for (unsigned j = 0; j < num_channels; ++j) {
+            float value = zero_start ? 0 : distribution(generator);
+            for (unsigned k = 0; k < series_len; ++k) {
+                value += distribution(generator);
+                // Cast the reference to `value` into `const char` pointer, so `outfile.write` will
+                // try to write the bytes stored in `value` as chars. By definition a `char` contains
+                // a single byte, so `sizeof(value)` can be used to specify how many chars to write.
+                outfile.write(reinterpret_cast<const char *>(&value), sizeof(value));
+            }
+        }
+    }
+
+    outfile.close();
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,95 @@
 #include <print>
 #include <string>
+#include <fstream>
 
 #include <CLI/CLI.hpp>
+#include "Util/RandomWalk.hpp"
 
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     CLI::App app{"Run ULISSE-MTS"};
 
     // Add subcommands
+    auto create_ds = app.add_subcommand("create_ds", "Create random walk dataset");
+    auto create_qs = app.add_subcommand("create_qs", "Create queries from dataset");
     auto index = app.add_subcommand("index", "Construct ULISSE MTS index");
     auto search = app.add_subcommand("search", "Search using ULISSE MTS");
     app.require_subcommand(1);
 
-    // Shared arguments
+    // Define custom validators
+    auto positive_int = CLI::Validator(
+        [](std::string &input) {
+            try {
+                unsigned value = std::stoi(input);
+                if (value > 0) {
+                    return "";
+                } else {
+                    return "Value must be greater than 0";
+                }
+            } catch (const std::exception &) {
+                return "Could not convert";
+            }
+        },
+        "POSITIVE_INTEGER", "Positive Integer");
+
+    // Add arguments
+    /*
+               | create_ds | create_query | index | search |
+    dataset    |     X     |       X      |   X   |        |
+    rw_noise   |     X     |              |       |        |
+    zero_start |     X     |              |       |        |
+    n          |     X     |              |       |        |
+    m          |     X     |       X      |   X   |        |
+    c          |     X     |       X      |   X   |        |
+    m_q        |           |       X      |       |        |
+    q_noise    |           |       X      |       |        |
+    l_min      |           |       X      |   X   |        |
+    l_max      |           |       X      |   X   |        |
+    s          |           |              |   X   |        |
+    n_start    |           |              |   X   |        |
+    leaf_th    |           |              |   X   |        |
+    approx/ex  |           |              |       |   X    |
+    kNN/r-ran  |           |              |       |   X    |
+    k(NN)      |           |              |       |   X    |
+    r(range)   |           |              |       |   X    |
+    normalize  |           |              |       |   X    |
+    out        |           |              |       |   X    |
+    */
+
     string dataset_path;
-    for (auto subcommand : {index, search}) {
+    create_ds->add_option("-d,--dataset", dataset_path, "Output dataset path")->required();
+    for (auto subcommand : {create_qs, index}) {
         subcommand->add_option("-d,--dataset", dataset_path, "Dataset to use")->required()->check(CLI::ExistingFile);
     }
 
-    // Indexation arguments
-    string index_out_path;
-    index->add_option("-o,--index_out", index_out_path, "Out path of the index file")->required();
+    float rw_noise = 1.0;
+    create_ds->add_option("-r,--rw_noise", rw_noise, "Random walk standard deviation")->capture_default_str();
 
-    // Search arguments
-    bool approximate_search = false;
-    search->add_flag("-a,--approximate", approximate_search, "Do approximate search");
+    bool zero_start = false;
+    create_ds->add_flag("-z,--zero_start", zero_start, "Start the random walk from zero");
+
+    unsigned num_series, series_len, num_channels;
+
+    create_ds->add_option("-n,--num_series", num_series, "Number of series")->required()->check(positive_int);
+    for (auto subcommand : {create_ds, create_qs, index}) {
+        subcommand->add_option("-m,--series_len", series_len, "Length of series")->required()->check(positive_int);
+        subcommand->add_option("-c,--num_channels", num_channels, "Number of channels")
+            ->required()
+            ->check(positive_int);
+    }
 
     // Execute command
     CLI11_PARSE(app, argc, argv);
 
-    if (index->parsed()) {
-        println("Preparing to index {}", dataset_path);
+    if (create_ds->parsed()) {
+        create_random_walks(dataset_path, rw_noise, zero_start, num_series, series_len, num_channels);
+    } else if (create_qs->parsed()) {
+        println("Query creation not implemented");
+    } else if (index->parsed()) {
+        println("Index creation not implemented");
     } else if (search->parsed()) {
-        println("Preparing to search on {}", dataset_path);
+        println("Search not implemented");
     }
 
     return 0;
