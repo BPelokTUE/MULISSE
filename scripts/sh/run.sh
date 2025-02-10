880cc48f93f7b03c805509ab2dfcb110db42c630
JdHondt <jensdhondt7@gmail.com>
2025-02-10 16:48:44 +0100

    changes to run.sh

--- scripts/sh/run.sh
+++ scripts/sh/run.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-n_series=500
-series_len=512
+n_series=5000
+series_len=2048
 n_channels=4
-n_queries=100
+n_queries=5
 base_dir=../DATA
 inner_dir=mts
-l_min=128
-l_max=512
-lengths=(128 256 512)
+l_min=1536
+l_max=2048
+lengths=(1536 1792 2048)
 k=1
 
 cd build
 mkdir -p ${base_dir}/${inner_dir}
 
+
 # Create random walk dataset
-# ./mulisse create_ds -d ${dataset_path} -n ${n_series} -m ${series_len} -c ${n_channels} -S 8999
+filename=rwalk_n${n_series}_m${series_len}_c${n_channels}
+dataset_path=${inner_dir}/${filename}.bin
+./mulisse create_ds -d ${dataset_path} -n ${n_series} -m ${series_len} -c ${n_channels} -S 8999
 
 # OR
 # Create dataset from CSV files
-csv_dir=/home/jens/tue/data/MTS/subsequence_search/preprocessed/weather
-csv_files=($(ls ${csv_dir}/*))
-num_channels=${#csv_files[@]}
+# csv_dir=/home/jens/tue/data/MTS/subsequence_search/preprocessed/weather
+# csv_files=($(ls ${csv_dir}/*))
+# num_channels=${#csv_files[@]}
+# filename=$(basename ${csv_dir})_n${n_series}_m${series_len}_c${num_channels}
 
 # Get basename from csv_dir
-filename=$(basename ${csv_dir})_n${n_series}_m${series_len}_c${num_channels}
+# filename=$(basename ${csv_dir})_n${n_series}_m${series_len}_c${num_channels}
 dataset_path=${inner_dir}/${filename}.bin
-index_path=${inner_dir}/index_${filename}.bin
+index_path=${inner_dir}/index_${filename}_lmin${l_min}_lmax${l_max}.bin
 fft_path=${inner_dir}/ffts_${filename}.bin
 query_path=${inner_dir}/query_${filename}.txt
 
@@ -35,11 +39,11 @@ query_path=${inner_dir}/query_${filename}.txt
 
 # Create queries
 echo "CREATING QUERIES"
-./mulisse create_qs -d ${dataset_path} -q ${query_path} -m ${series_len} -c ${n_channels} --lengths ${lengths[@]} --noise 0.1 -Q ${n_queries}
+./mulisse create_qs -d ${dataset_path} -q ${query_path} -m ${series_len} -c ${n_channels} --lengths ${lengths[@]} --noise 0.1 -Q ${n_queries} -u ${n_channels}
 
 # Create index for ulisse (and creates ffts)
 echo "CREATING INDEX"
-./mulisse index -d ${dataset_path} -i ${index_path} -m ${series_len} -c ${n_channels} -l ${l_min} -L ${l_max} -s 32 -p 385 -C 64  -F ${fft_path} -S em 
+./mulisse index -d ${dataset_path} -i ${index_path} -m ${series_len} -c ${n_channels} -l ${l_min} -L ${l_max} -s 32 -p $((l_max - l_min + 1)) -C 64  -F ${fft_path} -S em 
 
 ed_file=${inner_dir}/ed.txt
 mass_file=${inner_dir}/mass.txt
@@ -49,8 +53,8 @@ isax_mass_file=${inner_dir}/isax_mass.txt
 isax_mass_fft_file=${inner_dir}/isax_mass_fft.txt
 
 # BRUTE FORCE
-echo "BRUTE FORCE"
-./mulisse search -q ${query_path} -o ${ed_file} -d ${dataset_path} -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -t scan
+# echo "BRUTE FORCE"
+# ./mulisse search -q ${query_path} -o ${ed_file} -d ${dataset_path} -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -t scan
 
 # ED with early abandoning
 # echo "ED with early abandoning"
@@ -69,8 +73,8 @@ echo "MASS with precomputed FFTs"
 # ./mulisse search -q ${query_path} -o ${isax_ed_file} -d ${dataset_path} -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -i ${index_path}
 
 # ISAX ED with early abandoning
-echo "MULISSE ED with early abandoning"
-./mulisse search -q ${query_path} -o ${isax_ed_file} -d ${dataset_path} -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -i ${index_path} --early_abandon
+# echo "MULISSE ED with early abandoning"
+# ./mulisse search -q ${query_path} -o ${isax_ed_file} -d ${dataset_path} -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -i ${index_path} --early_abandon
 
 # ISAX MASS
 # echo "MULISSE MASS"
