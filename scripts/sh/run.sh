8478567a8d72c1d0600ec4d1b83e9e4bfab0280c
JdHondt <jensdhondt7@gmail.com>
2025-02-10 09:25:15 +0100

    added commets

--- scripts/sh/run.sh
+++ scripts/sh/run.sh
@@ -15,8 +15,13 @@ query_file=${inner_dir}/test_query.txt
 cd build
 mkdir -p ${base_dir}/${inner_dir}
 
+# Create dataset
 ./mulisse create_ds -d ${inner_dir}/test.bin -n ${n_series} -m ${series_len} -c ${n_channels} -S 8999
+
+# Create queries
 ./mulisse create_qs -d ${inner_dir}/test.bin -q ${inner_dir}/test_query.txt -m ${series_len} -c ${n_channels} --lengths ${lengths[@]} --noise 0.1 -Q ${n_queries}
+
+# Create index for ulisse (and creates ffts)
 ./mulisse index -d ${inner_dir}/test.bin -i ${inner_dir}/test_ind.bin -m ${series_len} -c ${n_channels} -l ${l_min} -L ${l_max} -s 32 -p 385 -C 64  -F ${inner_dir}/test_ffts.bin -S em 
 
 ed_file=${inner_dir}/ed.txt
@@ -26,9 +31,20 @@ isax_ed_file=${inner_dir}/isax_ed.txt
 isax_mass_file=${inner_dir}/isax_mass.txt
 isax_mass_fft_file=${inner_dir}/isax_mass_fft.txt
 
+# BRUTE FORCE
 ./mulisse search -q ${query_file} -o ${ed_file} -d ${inner_dir}/test.bin -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -t scan
+
+# MASS
 ./mulisse search -q ${query_file} -o ${mass_file} -d ${inner_dir}/test.bin -T knn -k ${k} -D mass -c ${n_channels} -m ${series_len} -t scan
+
+# MASS with precomputed FFTs
 ./mulisse search -q ${query_file} -o ${mass_fft_file} -d ${inner_dir}/test.bin -T knn -k ${k} -D mass -F ${inner_dir}/test_ffts.bin -c ${n_channels} -m ${series_len} -t scan
+
+# ISAX ED
 ./mulisse search -q ${query_file} -o ${isax_ed_file} -d ${inner_dir}/test.bin -T knn -k ${k} -D ed -c ${n_channels} -m ${series_len} -i ${inner_dir}/test_ind.bin
+
+# ISAX MASS
 ./mulisse search -q ${query_file} -o ${isax_mass_file} -d ${inner_dir}/test.bin -T knn -k ${k} -D mass -c ${n_channels} -m ${series_len} -i ${inner_dir}/test_ind.bin
+
+# ISAX MASS with precomputed FFTs
 ./mulisse search -q ${query_file} -o ${isax_mass_fft_file} -d ${inner_dir}/test.bin -T knn -k ${k} -D mass -c ${n_channels} -m ${series_len} -i ${inner_dir}/test_ind.bin -F ${inner_dir}/test_ffts.bin
