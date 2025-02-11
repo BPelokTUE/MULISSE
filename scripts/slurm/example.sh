185f2990e2feaa74988517c92aa960647b7d8722
JdHondt <jensdhondt7@gmail.com>
2025-02-11 07:49:00 +0100

    updated slurm example

--- scripts/slurm/example.sh
+++ scripts/slurm/example.sh
@@ -1,8 +1,13 @@
-module load 2024
-module load GCCcore/12.3.0
-module load CMake/3.26.3-GCCcore-12.3.0
-module load Ninja/1.11.1-GCCcore-12.3.0
-module load Boost/1.82.0-GCC-12.3.0
+#!/bin/bash
 
-module load eb/4.9.4
-eblocalinstall FFTW-3.3.10-GCC-12.3.0.eb
+module load 2023 &&
+module load GCCcore/12.3.0 &&
+module load CMake/3.26.3-GCCcore-12.3.0 &&
+module load Ninja/1.11.1-GCCcore-12.3.0 &&
+module load Boost/1.82.0-GCC-12.3.0 &&
+module load FFTW/3.3.10-GCC-12.3.0 &&
+
+# module load eb/4.9.4 &&
+# eblocalinstall FFTW-3.3.10-GCC-12.3.0.eb &&
+
+./scripts/sh/build.sh
